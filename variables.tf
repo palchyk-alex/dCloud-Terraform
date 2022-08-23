9c82ad4652425d8bf5b1d97f42e7a74a371e899d
Alex <43248561+palchyk-alex@users.noreply.github.com>
2022-08-23 09:59:24 +0300

    Change default value for username

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "pm_user" {
   description = "The username for the proxmox user"
   type        = string
   sensitive   = false
-  default     = "root@pam"
+  default     = ""
 }
 
 variable "pm_password" {
